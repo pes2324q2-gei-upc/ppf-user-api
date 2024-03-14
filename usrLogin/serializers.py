c1b76fe77c11cf2b63fe4fdeef088ac4036f88fd
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-03-14 19:14:57 +0100

    update for userLogin serializers

--- usrLogin/serializers.py
+++ usrLogin/serializers.py
@@ -1,17 +1,38 @@
+"""
+The serializers for the user login
+    Returns:
+        _type_: _description_
+"""
+
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
+from ppf.common.models.user import User, Token
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
+    """
+    the serializer for user login
+    """
     class Meta:
+        """Meta class for user login serializer
+        """
         model = User
         fields = ['username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user login
+
+    Args:
+        serializers (ModelSerializer): basic serializer model
+
+    Returns:
+        _type_: _description_
+    """
     class Meta:
+        """meta class for user serializer
+        """
         model = User
         fields = ['username', 'password']  # Add other fields as needed
         extra_kwargs = {'password': {'write_only': True}}
@@ -22,6 +43,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class TokenSerializer(serializers.ModelSerializer):
+    """
+    serializer class for the tokens
+    """
     class Meta:
+        """
+        meta class for the tokens
+        """
         model = Token
         fields = '__all__'

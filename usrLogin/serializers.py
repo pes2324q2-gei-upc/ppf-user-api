5febb331c84c9fd2ea6d3729713980fb6210ae5d
LightBlue247 <zongjun1031@gmail.com>
2024-03-14 18:49:00 +0100

    [#31] Version con expiracion de tokens

--- usrLogin/serializers.py
+++ usrLogin/serializers.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 
-class UserLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for handling user login data.
+
+    Args:
+        Serializer: Base class for serializers in Django REST Framework.
+    """
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128, write_only=True)
 
 
 class UserSerializer(serializers.ModelSerializer):

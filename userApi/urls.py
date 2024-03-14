e22c9137d134c503236a39018bb6e82f42a2cb61
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-03-14 19:51:25 +0100

    update on urls

--- userApi/urls.py
+++ userApi/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from api import urls as apiUrls
+from usrLogin import urls as userLoginUrls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("user-login", include(userLoginUrls)),
     path("", include(apiUrls))
 ]

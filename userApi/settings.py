0f912a71b0ea3cab45f725f313f0675cc28b9571
LightBlue247 <zongjun1031@gmail.com>
2024-03-15 23:59:40 +0100

    [#31] Auth con email

--- userApi/settings.py
+++ userApi/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'usrLogin.backends.EmailBackend',
     ],
 }

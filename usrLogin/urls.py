460ec2cc47cd90925ae625cc3ddfee51ab38455c
LightBlue247 <zongjun1031@gmail.com>
2024-03-21 09:55:53 +0100

    Cambiado estructura swagger urls al urls principal

--- usrLogin/urls.py
+++ usrLogin/urls.py
@@ -15,28 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from usrLogin import views
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version='v1',
-        description="Documentation for your API",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('login/', views.LoginAPIView.as_view(), name='login'),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
 ]

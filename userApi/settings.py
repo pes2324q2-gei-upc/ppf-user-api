85ec00e7ed05a04adf117b4cf45902741b9842e7
LightBlue247 <zongjun1031@gmail.com>
2024-03-22 01:07:09 +0100

    Quitado la clase Token, usa el del authToken

--- userApi/settings.py
+++ userApi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "common",
     'rest_framework',
+    'rest_framework.authtoken',
     "api",
     "usrLogin",
     'drf_yasg',
--- usrLogin/views.py
+++ usrLogin/views.py
@@ -5,10 +5,10 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from ppf.common.models.user import Token
 from .serializers import UserLoginSerializer
 
 # Create your views here.

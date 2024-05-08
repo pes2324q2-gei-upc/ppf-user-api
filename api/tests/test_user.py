c3f6bdc4c78674bc0a42a67d470c08b063fac35d
LightBlue247 <zongjun1031@gmail.com>
2024-05-08 20:45:37 +0200

    Todos los tests de user

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -1,3 +1,4 @@
+from urllib import response
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -220,3 +221,304 @@ class GetUserTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         message = json.loads(response.content.decode("utf-8"))
         self.assertEqual(message.get("detail"), "Not found.")
+
+
+class UpdateUserTest(APITestCase):
+    """
+    Test Update User
+    """
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+
+    def testSuccessfulUpdateUser(self):
+        """
+        Ensure the API call updates the user information.
+        """
+
+        url = reverse("userRetriever", kwargs={"id": self.user.pk})
+        dataPut = {
+            "username": "newUsername",
+            "first_name": "newFirstName",
+            "last_name": "newLastName",
+            "birthDate": "1998-10-06",
+            "email": "newUsername@gmail.com",
+        }
+        headers = {
+            "Authorization": f"Token {self.token}",
+        }
+        # Complete PUT
+        response = self.client.put(url, dataPut, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("id"), self.user.pk)
+        self.assertEqual(message.get("username"), "newUsername")
+        self.assertEqual(message.get("first_name"), "newFirstName")
+        self.assertEqual(message.get("last_name"), "newLastName")
+        self.assertEqual(message.get("birthDate"), "1998-10-06")
+        self.assertEqual(message.get("email"), "newUsername@gmail.com")
+
+        # Partial PUT
+        dataPut2 = {"username": "newUsername2"}
+        response = self.client.put(url, dataPut2, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("id"), self.user.pk)
+        self.assertEqual(message.get("username"), "newUsername")
+        self.assertEqual(message.get("first_name"), self.user.first_name)
+        self.assertEqual(message.get("last_name"), self.user.last_name)
+        self.assertEqual(message.get("birthDate"), self.user.birthDate)
+        self.assertEqual(message.get("email"), self.user.email)
+
+        # Partial PATCH
+        dataPatch = {
+            "username": "newUsername",
+        }
+        response = self.client.patch(url, dataPatch, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("id"), self.user.pk)
+        self.assertEqual(message.get("username"), "newUsername")
+        self.assertEqual(message.get("first_name"), self.user.first_name)
+        self.assertEqual(message.get("last_name"), self.user.last_name)
+        self.assertEqual(message.get("birthDate"), self.user.birthDate)
+        self.assertEqual(message.get("email"), self.user.email)
+
+        # Complete PATCH
+        dataPatch2 = {
+            "username": "newUsername2",
+            "first_name": "newFirstName2",
+            "last_name": "newLastName2",
+            "birthDate": "1998-10-06",
+            "email": "newUsername2@gmail.com",
+        }
+        response = self.client.patch(url, dataPatch2, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("id"), self.user.pk)
+        self.assertEqual(message.get("username"), "newUsername2")
+        self.assertEqual(message.get("first_name"), "newFirstName2")
+        self.assertEqual(message.get("last_name"), "newLastName2")
+        self.assertEqual(message.get("birthDate"), "1998-10-06")
+        self.assertEqual(message.get("email"), "newUsername2@gmail.com")
+
+    def testEmailCannotBeUpdated(self):
+        """
+        Ensure the API call returns an error if the user tries to update the email.
+        """
+
+        url = reverse("userRetriever", kwargs={"id": self.user.pk})
+        data = {"email": "perdigon@gmail.com"}
+        headers = {
+            "Authorization": f"Token {self.token}",
+        }
+        responsePut = self.client.put(url, data, headers=headers)  # type: ignore
+        self.assertEqual(responsePut.status_code, status.HTTP_200_OK)
+        message = json.loads(responsePut.content.decode("utf-8"))
+        self.assertEqual(message.get("email"), self.user.email)
+        self.assertNotEqual(message.get("email"), "perdigon@gmail.com")
+
+    def testUnauthorizedUpdateUser(self):
+        """
+        Ensure the API call returns an error if the user is not authenticated.
+        """
+
+        url = reverse("userRetriever", kwargs={"id": self.user.pk})
+        data = {"username": "newUsername"}
+        responsePut = self.client.put(url, data)
+        self.assertEqual(responsePut.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(responsePut.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Authentication credentials were not provided.")
+
+        responsePatch = self.client.put(url, data)
+        self.assertEqual(responsePatch.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(responsePatch.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Authentication credentials were not provided.")
+
+    def testUserNotExists(self):
+        """
+        Ensure the API call returns an error if the user does not exist.
+        """
+
+        url = reverse("userRetriever", kwargs={"id": 1000})
+        data = {"username": "newUsername"}
+        headers = {
+            "Authorization": f"Token {self.token}",
+        }
+        responsePut = self.client.put(url, data, headers=headers)  # type: ignore
+        self.assertEqual(responsePut.status_code, status.HTTP_404_NOT_FOUND)
+        message = json.loads(responsePut.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Not found.")
+
+        responsePatch = self.client.patch(url, data, headers=headers)  # type: ignore
+        self.assertEqual(responsePatch.status_code, status.HTTP_404_NOT_FOUND)
+        message = json.loads(responsePatch.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Not found.")
+
+    def testUserCannotUpdateOtherUser(self):
+        """
+        Ensure the API call returns an error if the user tries to update another user.
+        """
+
+        user2 = User.objects.create(
+            username="test2", birthDate="1998-10-06", password="test2", email="test2@gmail.com"
+        )
+        url = reverse("userRetriever", kwargs={"id": user2.pk})
+        data = {"username": "newUsername"}
+        headers = {"Authorization": f"Token {self.token}"}
+        responsePut = self.client.put(url, data, headers=headers)  # type: ignore
+        self.assertEqual(responsePut.status_code, status.HTTP_403_FORBIDDEN)
+        message = json.loads(responsePut.content.decode("utf-8"))
+        self.assertEqual(message.get("error"), "You can only update your own user account.")
+
+        responsePatch = self.client.patch(url, data, headers=headers)  # type: ignore
+        self.assertEqual(responsePatch.status_code, status.HTTP_403_FORBIDDEN)
+        message = json.loads(responsePatch.content.decode("utf-8"))
+        self.assertEqual(message.get("error"), "You can only update your own user account.")
+
+
+class DeleteUserTest(APITestCase):
+    """
+    Test Delete User
+    """
+
+    def testSuccessfulDeleteUser(self):
+        """
+        Ensure the API call deletes the user.
+        """
+        user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        token, _ = Token.objects.get_or_create(user=user)
+
+        url = reverse("userRetriever", kwargs={"id": user.pk})
+        headers = {
+            "Authorization": f"Token {token}",
+        }
+        response = self.client.delete(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def testUnauthorizedDeleteUser(self):
+        """
+        Ensure the API call returns an error if the user is not authenticated.
+        """
+        user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+
+        url = reverse("userRetriever", kwargs={"id": user.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Authentication credentials were not provided.")
+
+    def testUserWhoDeleteNotExists(self):
+        """
+        Ensure the API call returns an error if the user does not exist.
+        """
+        user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        token, _ = Token.objects.get_or_create(user=user)
+
+        url = reverse("userRetriever", kwargs={"id": 1000})
+        headers = {
+            "Authorization": f"Token {token}",
+        }
+        response = self.client.delete(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Invalid token.")
+
+    def testUserDeletedNotExists(self):
+        """
+        Ensure the API call returns an error if the user does not exist.
+        """
+        user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        token, _ = Token.objects.get_or_create(user=user)
+
+        url = reverse("userRetriever", kwargs={"id": 1000})
+        headers = {
+            "Authorization": f"Token {token}",
+        }
+        response = self.client.delete(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Not found.")
+
+    def testUserCannotDeleteOtherUser(self):
+        """
+        Ensure the API call returns an error if the user tries to delete another user.
+        """
+        user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        token, _ = Token.objects.get_or_create(user=user)
+        user2 = User.objects.create(
+            username="test2", birthDate="1998-10-06", password="test2", email="test2@gmail.com"
+        )
+
+        url = reverse("userRetriever", kwargs={"id": user2.pk})
+        headers = {
+            "Authorization": f"Token {token}",
+        }
+        response = self.client.delete(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("error"), "You can only delete your own user account.")
+
+
+class GetSelfIDTest(APITestCase):
+    """
+    Test Get Self ID
+    """
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username="test", birthDate="1998-10-06", password="test", email="test@gmail.com"
+        )
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+
+    def testSuccessfulGetSelfID(self):
+        """
+        Ensure the API call returns the user information.
+        """
+
+        url = reverse("userIdRetriever")
+        headers = {
+            "Authorization": f"Token {self.token}",
+        }
+        response = self.client.get(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("user_id"), self.user.pk)
+
+    def testUserNotExists(self):
+        """
+        Ensure the API call returns an error if the user does not exist.
+        """
+
+        url = reverse("userIdRetriever")
+        headers = {
+            "Authorization": f"Token Bonito",
+        }
+        response = self.client.get(url, headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Invalid token.")
+
+    def testUnauthorizedGetSelfID(self):
+        """
+        Ensure the API call returns an error if the user is not authenticated.
+        """
+
+        url = reverse("userIdRetriever")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(message.get("detail"), "Authentication credentials were not provided.")

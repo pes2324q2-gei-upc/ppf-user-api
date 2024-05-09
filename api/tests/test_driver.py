aa2e527524e3af3c1a87760a18ef8189ee267faf
LightBlue247 <zongjun1031@gmail.com>
2024-05-09 15:46:43 +0200

    Formateo

--- api/tests/test_driver.py
+++ api/tests/test_driver.py
@@ -1,6 +1,7 @@
 """
 This module contains the tests for the driver.
 """
+
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -1,6 +1,7 @@
 """
 This module contains the tests for the users.
 """
+
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status

57874c3cf1544440dd0d1cd2f8f89dfdba75c3a0
LightBlue247 <zongjun1031@gmail.com>
2024-05-16 19:34:03 +0200

    Added Autentificacion

--- achievement/views.py
+++ achievement/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView
 from common.models.achievement import UserAchievementProgress
 from achievement.serializers import UserAchievementProgressSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserAchievementList(ListAPIView):
@@ -9,6 +11,8 @@ class UserAchievementList(ListAPIView):
     """
 
     serializer_class = UserAchievementProgressSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user_id = self.kwargs["id"]

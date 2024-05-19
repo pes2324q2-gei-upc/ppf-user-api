aea6dd781727070101785bb30e9f9636e7460e73
LightBlue247 <zongjun1031@gmail.com>
2024-05-19 21:16:30 +0200

    checkpoint

--- api/tests/test_valuation.py
+++ api/tests/test_valuation.py
@@ -37,6 +37,7 @@ class CreateValuationTest(APITestCase):
         self.driver.chargerTypes.add(self.mennekes)
         self.tokenDriver = Token.objects.create(user=self.driver)
         self.route = Route.objects.create(
+            driver_id=self.driver.pk,
             originLat=41.350450,
             originLon=2.132660,
             originAlias="SomeWhere",
@@ -60,7 +61,6 @@ class CreateValuationTest(APITestCase):
             "receiver": self.driver.pk,
             "route": self.route.pk,
             "rating": 5,
-            "comment": "Valuando driver",
         }
         headers = {
             "Authorization": f"Token {self.tokenUser}",
@@ -92,3 +92,43 @@ class CreateValuationTest(APITestCase):
         self.assertEqual(message.get("route"), valuation.route.pk)
         self.assertEqual(message.get("rating"), valuation.rating)
         self.assertEqual(message.get("comment"), valuation.comment)
+
+    def testReceiverNotExist(self):
+        """
+        Test to create a valuation with a receiver that does not exist.
+        """
+
+        url = reverse("valuationListCreate")
+        data = {
+            "receiver": 100,
+            "route": self.route.pk,
+            "rating": 5,
+            "comment": "Valuando driver",
+        }
+        headers = {
+            "Authorization": f"Token {self.tokenUser}",
+        }
+        response = self.client.post(url, data, format="json", headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertIn("Invalid receiver ID. User not found.", message.get("error"))
+
+    def testCannotValuateYourself(self):
+        """
+        Test to create a valuation with the same user as receiver.
+        """
+
+        url = reverse("valuationListCreate")
+        data = {
+            "receiver": self.user.pk,
+            "route": self.route.pk,
+            "rating": 5,
+            "comment": "Valuando myself",
+        }
+        headers = {
+            "Authorization": f"Token {self.tokenUser}",
+        }
+        response = self.client.post(url, data, format="json", headers=headers)  # type: ignore
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        message = json.loads(response.content.decode("utf-8"))
+        self.assertIn("You cannot rate yourself.", message.get("error"))
